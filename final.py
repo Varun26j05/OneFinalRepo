90955bee87cd0961e2fd23ad851e2128a9e78ee5
Varun26j05 <122362317+Varun26j05@users.noreply.github.com>
2023-04-08 15:38:06 +0530

    Create final.py

---
+++ final.py
@@ -0,0 +1,18 @@
+a = 5
+b = 6
+if a>b :
+  print(f'{a} is greater than {b}')
+else:
+  print(f'{b} is greater that {a}')
+print("Hello world")
+varun@varun MINGW64 /d/Coding/Git (master)
+$ git push origin master
+To https://github.com/Varun977/PythonProjects
+ ! [rejected]        master -> master (fetch first)
+error: failed to push some refs to 'https://github.com/Varun977/PythonProjects'
+hint: Updates were rejected because the remote contains work that you do
+hint: not have locally. This is usually caused by another repository pushing
+hint: to the same ref. You may want to first integrate the remote changes
+hint: (e.g., 'git pull ...') before pushing again.
+hint: See the 'Note about fast-forwards' in 'git push --help' for details.
+//remove these error causing thingy
